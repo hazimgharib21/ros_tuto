6b9bf0280f48f23775189203c897d7518cda81ea
hazimgharib21 <hazimgharib21@gmail.com>
2019-05-08 09:46:20 +0800

    add Makefile and launch, update talker and listener script

--- beginner_tutorials/scripts/listener.py
+++ beginner_tutorials/scripts/listener.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
+# Simple listener demo that subscribed messages from 'chatter' topic
+#
+# Create executable script
+#   chmod +x listener.py
+#
+# Run script (without launch file)
+#   rosrun beginner_tutorials listener.py
+#
+# Run script (with launch file)
+#   roslaunch beginner_tutorials talker_listener.launch
+
+
 import rospy
 from std_msgs.msg import String
 
+# Subscriber callback function
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 def listener():
+
+    # Initialize our node with name listener
     rospy.init_node('listener', anonymous=True)
+
+    # Node subscribes to the chatter topic which is of type String
+    # When new messages are received, callback is invoded with the
+    # message as the first argument
     rospy.Subscriber("chatter", String, callback)
 
+    # keeps the node from exiting untile the node has been shutdown
     rospy.spin()
 
 if __name__ == '__main__':
--- beginner_tutorials/scripts/talker.py
+++ beginner_tutorials/scripts/talker.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python
 
+# Simple talker demo that published std_msgs/String messages to the 'chatter' topic
+#
+# Create executable script
+#   chmod +x talker.py
+#
+# Run script (without launch file)
+#   rosrun beginner_tutorials talker.py
+#
+# Run script (with launch file)
+#   roslaunch beginner_tutorials talker_listener.launch
+
 import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+
+    # Initialize our node with name talker
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10)
+
+    # Node are publish to the chatter topic using the message type String
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+
+    # Looping at desired rate
+    # loop 10 times every second
+    rate = rospy.Rate(10) #10hz
+
+    # Check shutdown flag
     while not rospy.is_shutdown():
+
         hello_str = "hello world %s" % rospy.get_time()
+
+        # Message printed to the screen
+        # Message written to the Node's log file
+        # Message written to rosout
         rospy.loginfo(hello_str)
+
+        # Publish a String to our chatter topic
         pub.publish(hello_str)
+
+        # Sleep long enough to maintain desired rate
+        # Specify in rospy.Rate(10)
         rate.sleep()
 
 if __name__ == '__main__':
